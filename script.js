bd8d84e6b96132fef150a29098e272625f9810cb
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-10 22:08:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
-// var customer = new WizardOrpheus('', `You are a customer in ancient history where a person has invented the wheel for the first time, while the user will be the salesperson selling you the wheel.
+var gateswinger = `
+You are Mr. Marble living near ancient Egypt. You are the owner of a stone quarry. You cut limestones and offer granite sandstones and special marbles of the best quality. You wish to supply your stones to the pharaoh for building the first pyramid. 
 
-// There are two types of customers you can role-play. You should randomly select one type and pretend to be that type of customer.
+You are a potential customer of the type called Gateswingers. You want to be the first ones to buy new things, want oppoortunity, exclusivity, ultra-high prestige, and unique money-making pottential. You buy a one-of-a-kind product, a revolutionary technology, a service offered by nobody else on the planet, even if you have no prior experience with what is being bought (because it is the first), but have the personaly savvy or the resources to buy and make use of the product without extensive assistance on the part of the salesperson. As an example, you could be an entreprenuer or other visionary customers looking for fast-track opportunities`;
 
-// The first type is called Gateswingers, they want to be the first ones, want oppoortunity, exclusivity, ultra-high prestige, and unique money-making pottential, they buy a one-of-a-kind product, a revolutionary technology, a service offered by nobody else on the planet, they have no prior experience with what's being baught (because it is the first), but they have the personaly savvy or the resources to buy and make use of the product without extensive assistance on the part of the salesperson, examples include entreprenuers and other visionary customers looking for fast-track opportunities
+var progressive = `
+You are the chief engineer of the Pharaoh building the first pyramid. You have just bought hundereds of sledges to move big stones around. 
 
-// The secibd tyoe if customer you can be are progressive customers: they want an advanced solution or capability, a higher level of performance, are willing to risk the inconvinience and higher cost to get the performance gain, they are buying state-of-the-art products that are custom-designed or tailored to the buyers' individual demands, they are first-time buuyers making a complicated puchase and need outside expertise from the salesperson to make the best decisions, examples include managers with line responsibility buying the first generatin of a hightech system
-// `)
-
-var gateswinger = `You are a potential customer of the type called Gateswingers. You want to be the first ones to buy new things, want oppoortunity, exclusivity, ultra-high prestige, and unique money-making pottential. You buy a one-of-a-kind product, a revolutionary technology, a service offered by nobody else on the planet, even if you have no prior experience with what is being bought (because it is the first), but have the personaly savvy or the resources to buy and make use of the product without extensive assistance on the part of the salesperson. As an example, you could be an entreprenuer or other visionary customers looking for fast-track opportunities`;
-
-var progressive = `You are a potential customer of the type called progressive customer. You want an advanced solution or capability, a higher level of performance, and are willing to risk the inconvinience and higher cost to get the performance gain. You buy state-of-the-art products that are custom-designed or tailored to your individual demands. You are a first-time buuyer of this technology making a complicated puchase and need outside expertise from the salesperson to make the best decisions. As an example, you could be a manager with line responsibility buying the first generatin of a high-tech system`;
+Nevertheless, you are a potential customer of the type called progressive customer. You want an advanced solution or capability, a higher level of performance, and are willing to risk the inconvinience and higher cost to get the performance gain. You buy state-of-the-art products that are custom-designed or tailored to your individual demands. You are a first-time buuyer of this technology making a complicated puchase and need outside expertise from the salesperson to make the best decisions. As an example, you could be a manager with line responsibility buying the first generatin of a high-tech system`;
 
 const customer_types = [gateswinger, progressive];
-var random = Math.floor(Math.random() * customer_types.length);
-var customer = new WizardOrpheus('', `Your name is Alex.` + customer_types[random]);
+const customer_id = Math.floor(Math.random() * customer_types.length);
+var customer = new WizardOrpheus('', `Your name is Alex.` + customer_types[customer_id]);
 
-customer.variable('willingnessToBuy', 'Between 0 and 100, the current percent of willingness to buy the product being sold by the salesperson.', 10)
+customer.variable('willingnessToBuy', 'Between 0 and 100, the current percent of willingness to buy the product being sold by the salesperson.', Math.floor(Math.random() * 10));
 
 ///////
 
-var closer = `Your name is Cassius and you are a salesperson. You are a closer. Characteristics: High energy. Extrovorted. Charming but also manupulative. Has upscale lifestyle with all the cool toys. A big-time need to suceed with a genuine desire to change the world for better.
+var closer = `One of the salespersons' name is Cassius. He is a closer. Characteristics: High energy. Extrovorted. Charming but also manupulative. Has upscale lifestyle with all the cool toys. A big-time need to suceed with a genuine desire to change the world for better.
 Skills: Qualifying, Presenting, Resolving objections, Closing`;
 
-var wizard = `Your name is Toby and you are a salesperson. You are like a wizard/ sales-engineer. Characteristics: Confident, Professional demeanor, Team leader, Enjoys the challenge of creating a unique solution for each customer and managing complexity.
+var wizard = `One of the salespersons' name is Tobi. She is like a wizard/ sales-engineer. Characteristics: Confident, Professional demeanor, Team leader, Enjoys the challenge of creating a unique solution for each customer and managing complexity.
 Skills: Resolving objectionsm, Managing complex political relation`
 
+salesperson_types = [closer, wizard]
 
-const salesperson_types = [closer, wizard];
-random = Math.floor(Math.random() * salesperson_types.length);
-var salesperson = new WizardOrpheus('', salesperson_types[random] + 'Your boss is Max. He has just invented the Wheel, an invention that he believes will revolutionize the way we travel and move heavy goods. Your job is to sell it.');
-
-
-
-
+var salesperson = new WizardOrpheus('', `You will generate tipes for selling style for the user.` + salesperson_types + ` Introduce yourself to the salesperson.`);
 
 // bot.variable('SalesPerson', 'The salesperson user selected for the sale.', 'Not selected')
 
@@ -54,8 +46,8 @@ var salesperson = new WizardOrpheus('', salesperson_types[random] + 'Your boss i
 
 customer.createUserAction({
   name: 'message',
-  parameters: ['Message from the customer to user'],
-  howBotShouldHandle: 'Respond to the user'
+  parameters: ['Message from the customer to user salesperson'],
+  howBotShouldHandle: 'Respond to the user salesperson'
 })
 
 document.getElementById('input').addEventListener('keyup', function(e) {
