99a24411cfee913cf0763710baf8c2e9a4e73ad2
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-10 23:19:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var gateswinger = `
-You are Mr. Marble living near ancient Egypt. You are the owner of a stone quarry. You cut limestones and offer granite sandstones and special marbles of the best quality. You wish to supply your stones to the pharaoh for building the first pyramid. 
+You are Mr. Marble living near ancient Egypt. You are the owner of a stone quarry. You cut limestones and offer granite sandstones and special marbles of the best quality. 
 
 You are a potential customer of the type called Gateswingers. You want to be the first ones to buy new things, want oppoortunity, exclusivity, ultra-high prestige, and unique money-making pottential. You buy a one-of-a-kind product, a revolutionary technology, a service offered by nobody else on the planet, even if you have no prior experience with what is being bought (because it is the first), but have the personaly savvy or the resources to buy and make use of the product without extensive assistance on the part of the salesperson. As an example, you could be an entreprenuer or other visionary customers looking for fast-track opportunities`;
 
 var progressive = `
-You are the chief engineer of the Pharaoh building the first pyramid. You have just bought hundereds of sledges to move big stones around. 
+You are the chief engineer of the Pharaoh building the first pyramid. 
 
 Nevertheless, you are a potential customer of the type called progressive customer. You want an advanced solution or capability, a higher level of performance, and are willing to risk the inconvinience and higher cost to get the performance gain. You buy state-of-the-art products that are custom-designed or tailored to your individual demands. You are a first-time buuyer of this technology making a complicated puchase and need outside expertise from the salesperson to make the best decisions. As an example, you could be a manager with line responsibility buying the first generatin of a high-tech system`;
 
 const customer_types = [gateswinger, progressive];
 const customer_id = Math.floor(Math.random() * customer_types.length);
-var customer = new WizardOrpheus('', `Your name is Alex.` + customer_types[customer_id]);
+var customer = new WizardOrpheus('', `Your name is Alex.` + customer_types[customer_id] + ` Introduce yourself to the salesperson in one line. Don't explicitly share your willingness to buy the product.`);
 
 customer.variable('willingnessToBuy', 'Between 0 and 100, the current percent of willingness to buy the product being sold by the salesperson.', Math.floor(Math.random() * 10));
 
@@ -24,7 +24,7 @@ Skills: Resolving objectionsm, Managing complex political relation`
 
 salesperson_types = [closer, wizard]
 
-var salesperson = new WizardOrpheus('', `You will generate tipes for selling style for the user.` + salesperson_types + ` Introduce yourself to the salesperson.`);
+var salesperson = new WizardOrpheus('', `You will generate tipes for selling style for the user.` + salesperson_types);
 
 // bot.variable('SalesPerson', 'The salesperson user selected for the sale.', 'Not selected')
 
@@ -46,22 +46,37 @@ var salesperson = new WizardOrpheus('', `You will generate tipes for selling sty
 
 customer.createUserAction({
   name: 'message',
-  parameters: ['Message from the customer to user salesperson'],
-  howBotShouldHandle: 'Respond to the user salesperson'
+  parameters: ['Message from the customer to user'],
+  howBotShouldHandle: 'Respond to the user'
 })
 
+var customerResponse;
+customer.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
+  // Add the bot's response to the conversation
+  customerResponse = data.message;
+  document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
+})
+
+salesperson.createUserAction({
+  name: 'context',
+  parameters: ['The current state of the conversation'],
+  howBotShouldHandle: 'Give tips to the user by analyzing the conversation. '
+})
+
+var userInput;
 document.getElementById('input').addEventListener('keyup', function(e) {
   if (e.code == 'Enter') { // if the user presses enter
-    let userInput = document.getElementById('input').value
+    userInput = document.getElementById('input').value
     customer.message(userInput)
 
     document.getElementById('conversation').innerHTML += '<p>' + userInput + '</p>'
-
     document.getElementById('input').value = ''
   }
 })
 
-customer.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
-  // Add the bot's response to the conversation
-  document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
+
+
+salesperson.context('User input:' + userInput + 'Customer response:' + customerResponse + '. Share tips on how to respond.')
+salesperson.botAction('tips', 'Share tips to the user on how to sell to the customer', { message: 'What tips you want to give to the user' }, data => {
+  document.getElementById('conversation').innerHTML += '<p>' + '<i>Tips:</i>'+ data.message + '</p>'
 })
