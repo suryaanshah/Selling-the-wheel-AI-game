15e05c371588a28c2740397c128de17e5011cca6
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-12 14:36:58 +0530

    add fast-growth prompt

--- technology.js
+++ technology.js
@@ -1,1 +1,4 @@
-var birth = "The 'birth' type of technology is new and revolutionary yet primitive. Products are capable of only basic tasks and have few if any features or options. During this period of infancy, apeal and value are limited to a relatively small number of people; but if the technology is successful, those few quickly become enthisiasts.
+var birth = "The 'birth' type of technology is new and revolutionary yet primitive. Products are capable of only basic tasks and have few if any features or options. During this period of infancy, apeal and value are limited to a relatively small number of people; but if the technology is successful, those few quickly become enthisiasts. As an example, when the wheel is invented, it is at this 'birth' stage initially."
+
+
+var fast_growth = "As the technology advances, it becomes the type 'fast growth'. This advance often happens dramatically and in big jumps. However, witht the advancement, there is an increase in options available and the complexity of the technology."
