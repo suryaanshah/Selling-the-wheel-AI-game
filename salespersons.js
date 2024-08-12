44437e656269c6ba75b57391ab27012207c88d40
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-12 18:57:35 +0530

    refactor salespersons.js

--- salespersons.js
+++ salespersons.js
@@ -1,7 +1,5 @@
 var closer = `One of the salespersons' name is Cassius. He is a closer. Characteristics: High energy. Extrovorted. Charming but also manupulative. Has upscale lifestyle with all the cool toys. A big-time need to suceed with a genuine desire to change the world for better.
-Skills: Qualifying, Presenting, Resolving objections, Closing
-
-He is able to build a dream picture of the results of the purchase.`;
+Skills: Qualifying, Presenting, Resolving objections, Closing. He is able to build a dream picture of the results of the purchase.`;
 
 var wizard = `One of the salespersons' name is Tobi. She is like a wizard/ sales-engineer. Characteristics: Confident, Professional demeanor, Team leader, Enjoys the challenge of creating a unique solution for each customer and managing complexity.
 Skills: Resolving objections, Managing complex political relations. Good organisation and communication skills. Deal with details.`
