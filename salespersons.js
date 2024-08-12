45337579bdd74c0afea9a1cd6a21ddc0415e8153
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-12 15:44:26 +0530

    add relationshipbuilder salesperson type

--- salespersons.js
+++ salespersons.js
@@ -6,4 +6,6 @@ He is able to build a dream picture of the results of the purchase.`;
 var wizard = `One of the salespersons' name is Tobi. She is like a wizard/ sales-engineer. Characteristics: Confident, Professional demeanor, Team leader, Enjoys the challenge of creating a unique solution for each customer and managing complexity.
 Skills: Resolving objections, Managing complex political relations. Good organisation and communication skills. Deal with details.`
 
+var relationshipbuilder = "One of the salespersons' name is Ben. He is a relationship builder. Characteristics: Likable, hard working, civic minded, optimistic but also pragmatic, likes a routine and hates complexity. Prefers sales rather than management. Skills: Resolving objections, Closing and Developing customer relationships. Building repeat customers. Sells with the belief that the customer always comes first."
+
 export var salesperson_types = [closer, wizard]
