29488a937b6327ecad7493e774f3a33a77f90654
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-12 18:59:00 +0530

    Update sellingstrategy.js

--- sellingstrategy.js
+++ sellingstrategy.js
@@ -1,6 +1,6 @@
 var strategy_closer = "The strategy used by closers can capture only one percent of the total pottential market. They are most suitable to sell gateswingers who buy technology of the 'birth' type. During market entry, expect an ice-cold reaction from most. But if you have the right product, the energy and the drive, and the capital to go the distance; this is an auspicious phase of the market cycle to start a business. There is little or no direct competition. One can own the market for a time. Core selling strategy: Build the dream. Play to the customer's fantasy and desires. Use excitement to conquer the customer's fear."
 
-var strategy_wizard = ""
+var strategy_wizard = "The strategy used by wizards/ sales-engineer can caputre upto 20 percent of the total pottential market. They are most suitable to sell to progressive customers who buy technology of the fast growth type. During market entry, expect the cool response from most prospects. A cutting edge technology that is developed and sophisticated is needed. Some will welcome the new capability and technology, but many will question the necessity of it, or will object to the cost involved."
 
 var var strategy_relationshipbuilder = ""
 
