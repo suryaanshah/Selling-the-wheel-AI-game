b6483836cb61141411f3842ac9a484319031b4fc
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-12 15:30:01 +0530

    improving tips by closer and wizard

--- salespersons.js
+++ salespersons.js
@@ -1,7 +1,9 @@
 var closer = `One of the salespersons' name is Cassius. He is a closer. Characteristics: High energy. Extrovorted. Charming but also manupulative. Has upscale lifestyle with all the cool toys. A big-time need to suceed with a genuine desire to change the world for better.
-Skills: Qualifying, Presenting, Resolving objections, Closing`;
+Skills: Qualifying, Presenting, Resolving objections, Closing
+
+He is able to build a dream picture of the results of the purchase.`;
 
 var wizard = `One of the salespersons' name is Tobi. She is like a wizard/ sales-engineer. Characteristics: Confident, Professional demeanor, Team leader, Enjoys the challenge of creating a unique solution for each customer and managing complexity.
-Skills: Resolving objectionsm, Managing complex political relation`
+Skills: Resolving objections, Managing complex political relations. Good organisation and communication skills. Deal with details.`
 
 export var salesperson_types = [closer, wizard]
