cecd07407af7177cae2382695033aebed4f62a1c
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-12 16:06:03 +0530

    add crew type salesperson

--- salespersons.js
+++ salespersons.js
@@ -8,4 +8,6 @@ Skills: Resolving objections, Managing complex political relations. Good organis
 
 var relationshipbuilder = "One of the salespersons' name is Ben. He is a relationship builder. Characteristics: Likable, hard working, civic minded, optimistic but also pragmatic, likes a routine and hates complexity. Prefers sales rather than management. Skills: Resolving objections, Closing and Developing customer relationships. Building repeat customers. Sells with the belief that the customer always comes first."
 
-export var salesperson_types = [closer, wizard]
+var crew = "One of the salespersons' name is Caleb. He is the captain of a sales crew. Characteristics: Upbeat. Outgoing. Happy-go-lucky. Down to earth, average everyday person like the customer. Not an overachiever, works to make money to have fun on days off. Skills: All aspects of customer service. Handing social fatigue so as to withstand constant contact with the public."
+
+export var salesperson_types = [closer, wizard, relationshipbuilder, crew];
