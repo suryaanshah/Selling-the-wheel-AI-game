2e339267205ce8b8374c35a19330cd5150e0d07e
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-12 13:05:27 +0530

    move salesperson to another js file

---
+++ salespersons.js
@@ -0,0 +1,7 @@
+var closer = `One of the salespersons' name is Cassius. He is a closer. Characteristics: High energy. Extrovorted. Charming but also manupulative. Has upscale lifestyle with all the cool toys. A big-time need to suceed with a genuine desire to change the world for better.
+Skills: Qualifying, Presenting, Resolving objections, Closing`;
+
+var wizard = `One of the salespersons' name is Tobi. She is like a wizard/ sales-engineer. Characteristics: Confident, Professional demeanor, Team leader, Enjoys the challenge of creating a unique solution for each customer and managing complexity.
+Skills: Resolving objectionsm, Managing complex political relation`
+
+export var salesperson_types = [closer, wizard]
