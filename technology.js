cde5e8c81fb756be04b6398e9cffd076f85da154
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-12 13:46:50 +0530

    add birth

--- technology.js
+++ technology.js
@@ -1,1 +1,1 @@
-
+var birth = "The 'birth' type of technology is new and revolutionary yet primitive. Products are capable of only basic tasks and have few if any features or options. During this period of infancy, apeal and value are limited to a relatively small number of people; but if the technology is successful, those few quickly become enthisiasts.
