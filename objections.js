1ea198988702b4b9ae2924a13b74128bcc0c4c43
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-13 16:17:28 +0530

    Create objections.js

---
+++ objections.js
@@ -0,0 +1,9 @@
+var objections_gateswinger = `Typical objections of a gateswinger type customer are: Should I spend so much money on something this new? Is this really going to get me the performance promised? Do I really need this new tech at all?`
+
+var objections_progressive = `Typical objections of a progressive type customer are: Does the benefit outweigh the cost and headaches of making a radical change? is this design really the best solution?`
+
+var objections_relationship = `Typical objections of a relationship type customer tend to revolve around the issues brought up by customer's in-house experts such as: Can you meet our special specifications and delivery requirements? Price is an issue but not the most important one.`
+
+var objections_world = `Typical objections of a world type customer are: is this the cheapest price? What happens if there is a problem after the sale?`
+
+export var objections = [objections_gateswinger, objections_progressive, objections_relationship, objections_world];
