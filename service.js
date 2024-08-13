26c44dbaeecf621756a730522a7549a2fab77021
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-13 15:18:43 +0530

    Add service relationshipbuilder

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
-var service_closer = `A closer offers a refund in the event of a major problem, but the customer is usally self-reliant with applying the technology, so service is not a determining factor for a sale.`
+export var service_closer = `A closer offers a refund in the event of a major problem, but the customer is usally self-reliant with applying the technology, so service is not a determining factor for a sale.`
 
-var service_wizard = `A wizard/sales engineer offers a total support service, doing evereything to ensure pronised results, taking measures to minimize disruptions to customer's operations. `
+export var service_wizard = `A wizard/sales engineer offers a total support service, doing evereything to ensure pronised results, taking measures to minimize disruptions to customer's operations. `
 
-var service_relationshipbuilder = ``
+export var service_relationshipbuilder = `A relationshipbuilder offers a service by being an advocate for the customer's needs, and expiditing the delivery of the order, and understanding each customer's unique preferences.`
 
-var service_crew = ``
+export var service_crew = ``
