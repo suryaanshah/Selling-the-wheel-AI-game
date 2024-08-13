f165c703577e90557c779b8edbd9938d483c15c0
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-13 15:09:04 +0530

    Create service.js and add service style of closer and wizard

---
+++ service.js
@@ -0,0 +1,7 @@
+var service_closer = `A closer offers a refund in the event of a major problem, but the customer is usally self-reliant with applying the technology, so service is not a determining factor for a sale.`
+
+service_wizard = `A wizard/sales engineer offers a total support service, doing evereything to ensure pronised results, taking measures to minimize disruptions to customer's operations. `
+
+service_relationshipbuilder = ``
+
+service_crew = ``
