7c045b75d24460a7ed6d9c5e4781635fe6be394f
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-13 16:25:33 +0530

    Several changes in script
    - Include objections in customer's response
    - improve prompting
    - add change interval to interest to buy
    - show technology stage context to user

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 import { customer_id, customer_types } from './customers.js';
 import { salesperson_types, salesperson_names } from './salespersons.js';
-import { technologies } from './technology.js';
+import { technologies, technology_stage_names } from './technology.js';
+import { objections } from './objections.js';
+
+
+document.getElementById('stagetech').innerHTML += '<p>' + '<b> Stage of wheel technology:</b><i> ' + technology_stage_names[customer_id] + "</i>. " + technologies
+    [customer_id] + "</p>"
 
 const initial_interest = Math.floor(Math.random() * 10)
 const initial_desired_price = [100000, 1000, 100, 10][customer_id];
 const initial_knowledge_about_wheel = [0, 25, 50, 75][customer_id];
 
-var customer = new WizardOrpheus('', `Your name is Alex.` + customer_types[customer_id] + `You live in ancient Egypt where the invention of the wheel is at the following stage: ` + technologies[customer_id] + ` When greeting, you introduce yourself in one line only. Your current level of knowledge about the wheels is at ` + initial_knowledge_about_wheel + ` out of a hundered. Your current interest in buying the wheels offered by the salesperson is at `+ initial_interest + ` percent. It increases or decreases gradually as the wheels technology is marketed to you.  You are initially willing to pay upto ` + initial_desired_price + ` units of currency per wheel. Negotiate the costs a little with the salesperson if needed as you become more interested.`);
+var customer = new WizardOrpheus('', `Your name is Alex.` + customer_types[customer_id] + `You live in ancient Egypt where the invention of the wheel is at the following stage: ` + technologies[customer_id] + ` When greeting, you introduce yourself in one line only. Your current level of knowledge about the wheels is at ` + initial_knowledge_about_wheel + ` out of a hundered. Your current interest in buying the wheels offered by the salesperson is at ` + initial_interest + ` percent. It increases or decreases with every interaction. ` + objections[customer_id] + `. You are initially willing to pay upto ` + initial_desired_price + ` units of currency per wheel. Negotiate the costs a little with the salesperson if needed as you become more interested.`);
 
-customer.variable('interestToBuy', 'Between 0 and 100, it is your current level of interest and enthusiasm to buy the product being sold by the salesperson.', initial_interest);
+customer.variable('interestToBuy', 'Between 0 and 100, it is your current level of interest and enthusiasm to buy the product being sold by the salesperson. Changes by approximately 5 percent with every interaction. Once above 89 percent, it will rarely go down.', initial_interest);
 
 customer.variable('pricePerWheel', 'Higher than 0, the price per item that you are willing to pay.', initial_desired_price);
 
