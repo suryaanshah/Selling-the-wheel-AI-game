c7b0d56f73a5d593853e46f76d2756d7aaab06c5
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-13 10:23:34 +0530

    Improve the prompt
    Incorporating interest to buy, initial price, and knowledge about the wheel in initial prompt

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ import { customer_id, customer_types } from './customers.js';
 import { salesperson_types, salesperson_names } from './salespersons.js';
 import { technologies } from './technology.js';
 
-var customer = new WizardOrpheus('', `Your name is Alex.` + customer_types[customer_id] + `You live in ancient Egypt where the invention of the wheel is at the following stage: ` + technologies[customer_id] + ` When greeting, you introduce yourself in one line only. See if you are interested in buying the wheel technology solution that the salesperson offers as the conversation progresses and increase or decrease your interest to buy the wheels accordingly. Negotiate the costs a little with the salesperson if needed as you become more interested.`);
-
 const initial_interest = Math.floor(Math.random() * 10)
 const initial_desired_price = [100000, 1000, 100, 10][customer_id];
 const initial_knowledge_about_wheel = [0, 25, 50, 75][customer_id];
+
+var customer = new WizardOrpheus('', `Your name is Alex.` + customer_types[customer_id] + `You live in ancient Egypt where the invention of the wheel is at the following stage: ` + technologies[customer_id] + ` When greeting, you introduce yourself in one line only. Your current level of knowledge about the wheels is at ` + initial_knowledge_about_wheel + ` out of a hundered. Your current interest in buying the wheels offered by the salesperson is at `+ initial_interest + ` percent. It increases or decreases gradually as the wheels technology is marketed to you.  You are initially willing to pay upto ` + initial_desired_price + ` units of currency per wheel. Negotiate the costs a little with the salesperson if needed as you become more interested.`);
 
 customer.variable('interestToBuy', 'Between 0 and 100, it is your current level of interest and enthusiasm to buy the product being sold by the salesperson.', initial_interest);
 
