d10421de951b6fdc89a2dbd67d289631aaca5f52
Suryaansh Chawla <suryaanshchawla@tutamail.com>
2024-08-13 22:16:38 +0530

    change prices and add potential customer in prompt

--- script.js
+++ script.js
@@ -5,19 +5,20 @@ import { objections } from './objections.js';
 
 
 document.getElementById('stagetech').innerHTML += '<p>' + '<b> Stage of wheel technology:</b><i> ' + technology_stage_names[customer_id] + "</i>. " + technologies
-    [customer_id] + "</p>"
+[customer_id] + "</p>"
 
 const initial_interest = Math.floor(Math.random() * 10)
-const initial_desired_price = [100000, 1000, 100, 10][customer_id];
+const initial_desired_price = [100000, 10000, 1000, 100][customer_id];
 const initial_knowledge_about_wheel = [0, 25, 50, 75][customer_id];
 
-var customer = new WizardOrpheus('', `Your name is Alex.` + customer_types[customer_id] + `You live in ancient Egypt where the invention of the wheel is at the following stage: ` + technologies[customer_id] + ` When greeting, you introduce yourself in one line only. Your current level of knowledge about the wheels is at ` + initial_knowledge_about_wheel + ` out of a hundered. Your current interest in buying the wheels offered by the salesperson is at ` + initial_interest + ` percent. It increases or decreases with every interaction. ` + objections[customer_id] + `. You are initially willing to pay upto ` + initial_desired_price + ` units of currency per wheel. Negotiate the costs a little with the salesperson if needed as you become more interested.`);
+var customer = new WizardOrpheus('', `Your name is Alex, a potential customer for purchasing wheels.` + customer_types[customer_id] + `You live in ancient Egypt where the invention of the wheel is at the following stage: ` + technologies[customer_id] + ` When greeting, you introduce yourself in one line only. Your current level of knowledge about the wheels is at ` + initial_knowledge_about_wheel + ` out of a hundered. Your current interest in buying the wheels offered by the salesperson is at ` + initial_interest + ` percent. It increases or decreases with every interaction. ` + objections[customer_id] + `. You are initially willing to pay upto ` + initial_desired_price + ` units of currency per wheel. Negotiate the costs a little with the salesperson if needed as you become more interested.`);
 
 customer.variable('interestToBuy', 'Between 0 and 100, it is your current level of interest and enthusiasm to buy the product being sold by the salesperson. Changes by approximately 5 percent with every interaction. Once above 89 percent, it will rarely go down.', initial_interest);
 
 customer.variable('pricePerWheel', 'Higher than 0, the price per item that you are willing to pay.', initial_desired_price);
 
 var salesperson = new WizardOrpheus('', `You will generate tips for selling for the user, based on the type of the customer.` + salesperson_types[customer_id] + " Share tips on how to drive the conversation forward. The aim is to maximize the selling price and the number of wheels sold. There can be a trade-off between the two, so give tips to negotiate with the customer appropriately.`");
+
 
 customer.createUserAction({
   name: 'message',
@@ -29,7 +30,7 @@ let customerResponse = '';
 customer.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
   // Add the bot's response to the conversation
   document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
-  document.getElementById('willingnessToBuy').innerText = data.currentVariables.interestToBuy.value
+  document.getElementById('willingnessToBuy').innerText = data.currentVariables.interestToBuy.value;
 
   document.body.style.backgroundColor = `rgba(0, 255, 0, ${data.currentVariables.interestToBuy.value * 1 / 100})`
 })
@@ -41,7 +42,7 @@ salesperson.createUserAction({
 })
 
 salesperson.botAction('tips', 'Give only one tip at a time to the salesperson by analyzing the conversation between salesperson and the customer. Understand who asked what and who said what and respond based on that.', { tip: 'What tips you want to give to the salesperson on making the sale.' }, data => {
-  document.getElementById('conversation').innerHTML += '<p>' + '=== <br> <i>' + salesperson_names[customer_id] + "'s tip: " + data.tip + "</i></p>"
+  document.getElementById('conversation').innerHTML += '<p>' + '=== <br> <i>' + salesperson_names[customer_id] + "'s tip: " + data.tip + "</i></p>";
 })
 
 let userInput = '';
